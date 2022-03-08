0a134c8e55b67561d926b56101bd705d6a43900a
Larry Manley Jr <97045752+LarryManleyJr@users.noreply.github.com>
2022-03-08 09:35:07 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/liftOff/LiftOffApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftOff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftOffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftOffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/liftOff/LiftOffApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.liftOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
