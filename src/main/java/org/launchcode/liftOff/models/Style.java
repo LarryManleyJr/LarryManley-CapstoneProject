226df657e7e4cc7d359a52db230a49b4fb6de1a7
Larry Manley Jr <97045752+LarryManleyJr@users.noreply.github.com>
2022-03-14 22:50:40 -0400

    March14done

--- src/main/java/org/launchcode/liftOff/models/Style.java
+++ src/main/java/org/launchcode/liftOff/models/Style.java
@@ -1,10 +1,30 @@
 package org.launchcode.liftOff.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Style extends AbstractEntity {
+
+    @ManyToMany
+    private List<Inventory>inventories =new ArrayList<>();
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+
+    public void setInventory(List<Inventory> inventory){
+        this.inventories = inventory;
+    }
+
+    public Style(){};
 }
+
 
 
